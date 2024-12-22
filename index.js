f394f4e9bae2f131e406b70ebf8d0cf91bfbe8ab
Infinper <141487969+Infiniper@users.noreply.github.com>
2024-12-22 23:04:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -52,16 +52,22 @@ rollButton.addEventListener('click', () => {
      var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
      document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
-     if (randomNumber1 > randomNumber2) {
-       document.querySelector("h1").textContent = "🚩 Player 1 Wins!";
+     if (randomNumber1 > randomNumber2) {     
        updateScore("player1");
      } else if (randomNumber2 > randomNumber1) {
-       document.querySelector("h1").textContent = "Player 2 Wins! 🚩";
        updateScore("player2");
      } else {
-       document.querySelector("h1").textContent = "Draw!";
        numberOfRolls++;
        document.getElementById("numberOfRolls").textContent = numberOfRolls;
      }
+     
+    if (scorePlayer1 > scorePlayer2) {
+       document.querySelector("h1").textContent = "🚩 Player 1 Wins!";
+     } else if (scorePlayer2 > scorePlayer1) {
+       document.querySelector("h1").textContent = "Player 2 Wins! 🚩";
+     } else {
+       document.querySelector("h1").textContent = "Draw!";
+     }
+     
    }
 
